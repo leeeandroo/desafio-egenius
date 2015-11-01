cb7727804f8987f3b137d4b18e5da1ceca13cb27
Leandro Vasco <leeeandroo@gmail.com>
2015-11-01 16:07:09 -0200

    update karma version

--- client/app/room/room.controller.spec.js
+++ client/app/room/room.controller.spec.js
@@ -18,6 +18,7 @@ describe('Controller: RoomCtrl', function () {
       .respond([{ 'name' : 'Room 01' }, { 'name' : 'Room 02' }]);
 
     scope = $rootScope.$new();
+    
     RoomCtrl = $controller('RoomCtrl', {
       $scope: scope
     });

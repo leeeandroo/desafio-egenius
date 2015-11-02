16b08f10092ff67437e251a362a7f70a1b5100f3
Leandro Vasco <leeeandroo@gmail.com>
2015-11-02 11:31:50 -0200

    Client tests

--- client/app/chat/chat.controller.spec.js
+++ client/app/chat/chat.controller.spec.js
@@ -3,16 +3,69 @@
 describe('Controller: ChatCtrl', function () {
 
   // load the controller's module
+  beforeEach(module('app/chat/chat.html'));
+  beforeEach(module('app/main/main.html'));
   beforeEach(module('chatApp'));
-
-  var ChatCtrl, scope;
+  beforeEach(module('socketMock'));
   
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    ChatCtrl = $controller('ChatCtrl', {
-      $scope: scope
-    });
-  }));
+  var ChatCtrl,
+      scope,
+      $httpBackend,
+      state,
+      id;
+  
+  
 
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope, $state, $stateParams) {
+    $httpBackend = _$httpBackend_;
+    
+    state = { 'params': { 'id': '562a752075c7cf5c1dbd6eb7' } };
+    
+    $httpBackend
+      .whenGET("/api/rooms/"+state.params.id)
+        .respond(function (method, url, data, headers) {
+            return [200, { 'name' : 'Room 01', '_id': '562a752075c7cf5c1dbd6eb7'  }, {}];
+        });  
+
+    $httpBackend
+      .whenGET("/api/chats/562a752075c7cf5c1dbd6eb7")
+        .respond(function (method, url, data, headers) {
+            return [200, [{ 'body' : 'Fake message', 'author': 'Fake author', 'date': Date.now(), 'room': '562a752075c7cf5c1dbd6eb7' }], {}];
+        });  
+
+    scope = $rootScope.$new();      
+
+    ChatCtrl = $controller('ChatCtrl', {
+      $scope: scope,
+      $state: state
+    });
+  }));  
+
+
+  it('should attach a list of messages to the scope', function () {
+    $httpBackend.flush();     
+    expect(scope.messages.length).toBe(1);    
+  });
+
+
+  it('should add new room to the scope', function () {
+    $httpBackend.flush();    
+    expect(scope.messages.length + 1).toBe(2);
+  });
+
+  
+  it('should a list of messages of determine room', function () {
+    $httpBackend.flush();  
+
+    for(var i = 0; i < scope.messages.length; i++) {
+      expect(scope.messages[i].room).toBe('562a752075c7cf5c1dbd6eb7');  
+    }  
+    
+  });
+
+
+  
 });
+
+
--- client/app/room/room.controller.spec.js
+++ client/app/room/room.controller.spec.js
@@ -3,30 +3,38 @@
 describe('Controller: RoomCtrl', function () {
 
   // load the controller's module
+  beforeEach(module('app/room/rooms.html'));
+  beforeEach(module('app/main/main.html'));
   beforeEach(module('chatApp'));
   beforeEach(module('socketMock'));
-  beforeEach(module('room.html'));
-
+  
   var RoomCtrl,
       scope,
       $httpBackend;
+      
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/rooms')
-      .respond([{ 'name' : 'Room 01' }, { 'name' : 'Room 02' }]);
 
     scope = $rootScope.$new();
+
     
+    $httpBackend
+      .whenGET("/api/rooms")
+        .respond(function (method, url, data, headers) {
+            return [200, [{ 'name' : 'Room 01' }, { 'name' : 'Room 02' }], {}];
+        });
+
     RoomCtrl = $controller('RoomCtrl', {
       $scope: scope
     });
   }));  
 
+
   it('should attach a list of rooms to the scope', function () {
-    $httpBackend.flush();    
-    expect(scope.rooms.length).toBe(2);
+    $httpBackend.flush();     
+    expect(scope.rooms.length).toBe(2);    
   });
 
 
